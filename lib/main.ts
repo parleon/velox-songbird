c8b7349cbeeb638e87560a8fff04b299f9143a6c
Justin Parsons <67924835+parleon@users.noreply.github.com>
2023-02-16 17:21:06 -0500

    done (#3)

--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,12 @@
+import { ChannelMessage } from "./interfaces";
 import { Velox } from "./velox";
 
 const config = require("./../build/config.json")
 
 const velox = new Velox(config.socketAddr)
+
+velox.onchannelopen((peer) => {
+    const cm: ChannelMessage = {Body:"Hello World"}
+    velox.send(cm)
+    velox.send({Body:peer}, [peer])
+})
--- lib/velox.ts
+++ lib/velox.ts
@@ -1,6 +1,6 @@
 import { Channel } from "./channel";
 import { Nest } from "./nest";
-import { ChannelMessage, RecievableNestMessageType, SendableNestMessage } from "./interfaces";
+import { BeaconEvent, ChannelMessage, ChannelMetaUpdate, RecievableNestMessage, RecievableNestMessageType, SendableNestMessage } from "./interfaces";
 
 export class Velox {
 
@@ -8,72 +8,120 @@ export class Velox {
     private _nest: Nest;
     private _activeChannels: Map<string, Channel>;
     private _beacon: EventTarget = new EventTarget();
-    private _channelNestBridge: (msg: SendableNestMessage) => void 
-    private _messageMap: Map<string, (cm: ChannelMessage) => void>
+    private _messageCallbackMap: Map<string, (cm: ChannelMessage) => void>
     private _defaultMessageCallback: (cm: ChannelMessage) => void
+    private _onChannelOpened: (UUID: string) => void = (UUID: string) => console.log(UUID + " Opened")
+    private _onChannelClosed: (UUID: string) => void = (UUID: string) => console.log(UUID + " Closed")
 
     constructor(socketAddr?: string) {
         this._activeChannels = new Map<string, Channel>();
-        this._messageMap = new Map<string, (cm: ChannelMessage) => void>;
+        this._messageCallbackMap = new Map<string, (cm: ChannelMessage) => void>;
         this._defaultMessageCallback = (cm) => {console.log(cm)}
 
-        this._nest = new Nest(socketAddr);
-
-
-        // function passed to a channel so it can communicate to nest
-        this._channelNestBridge = (msg: SendableNestMessage) => {
-            if (msg.UUID == null) {
-                msg = {...msg, UUID: this._UUID};
-            }
-            this._nest.send(msg)    
+        // channel uses this to emit RCM for velox to route to client
+        const RCMHandler = (message: ChannelMessage) => {
+            this._beacon.dispatchEvent(new CustomEvent("RCM", {detail: {CM: message}}))
         }
 
-        const _veloxConnector = (msg: ChannelMessage) => {
-            const fun = this._messageMap.get(msg.Type)
-            if (fun == undefined) {
-                this._defaultMessageCallback(msg)
-            } else {
-                fun(msg)
-            }
+        // channel uses this to emit SNM for velox to route to nest
+        const SNMHandler = (message: SendableNestMessage) => {
+            this._beacon.dispatchEvent(new CustomEvent("SNM", {detail: {SNM: message}}))  
         }
 
-        this._nest.addNestMessageProcess('recieved', (message) => {
+        // nest uses this to emit RNM for velox to route to channels
+        const RNMHandler = (message: RecievableNestMessage) => {
+            this._beacon.dispatchEvent(new CustomEvent<BeaconEvent>("RNM", {detail: {RNM: message}}))
+        }
+
+        // channel uses this to emit CMU for velox to handle or forward
+        const CMUHandler = (message: ChannelMetaUpdate) => {
+            this._beacon.dispatchEvent(new CustomEvent<BeaconEvent>("CMU", {detail: {CMU: message}}))
+        }
+
+        this._beacon.addEventListener("RNM", (event: CustomEvent<BeaconEvent>) => {
+
+            const message = event.detail.RNM;
+
+            // initial messages are used to configure velox metadata
             if (message.Type == RecievableNestMessageType.Initial) {
                 this._UUID = message.UUID   
+            
+            // the start handshake and offer messages create new channels and binds a listener to them for RNM routing
             } else if (message.Type == RecievableNestMessageType.StartHandshake || message.Type == RecievableNestMessageType.Offer) {
-                this._activeChannels.set(message.UUID, new Channel(this._channelNestBridge, _veloxConnector));
-                this._beacon.addEventListener(message.UUID, (event: CustomEvent) => {
-                    this._activeChannels.get(message.UUID).processNestMessage(event);
+                this._activeChannels.set(message.UUID, 
+                    new Channel(
+                        SNMHandler,
+                        RCMHandler,
+                        CMUHandler));
+                this._beacon.addEventListener(message.UUID, (event: CustomEvent<BeaconEvent>) => {
+                    this._activeChannels.get(message.UUID).RNMProcessor(event.detail.RNM);
                 })
-                this._beacon.dispatchEvent(new CustomEvent(message.UUID, {detail: message}))
+                this._beacon.dispatchEvent(new CustomEvent<BeaconEvent>(message.UUID, {detail: {RNM: message}}))
+            
+            // all other RNM are routed accordingly
             } else if (message.UUID != null) {
-                this._beacon.dispatchEvent(new CustomEvent(message.UUID, {detail: message}))
+                this._beacon.dispatchEvent(new CustomEvent<BeaconEvent>(message.UUID, {detail: {RNM: message}}))
             }
-        }
-        )
+        })
+
+        this._beacon.addEventListener("SNM", (event: CustomEvent<BeaconEvent>) => {
+            const message: SendableNestMessage = event.detail.SNM
+            if (message.UUID == null) {
+                this._nest.SNMProcessor({...message, UUID:this._UUID})
+            } else {
+                this._nest.SNMProcessor(message)
+            }
+        })
+
+        this._beacon.addEventListener("RCM", (event: CustomEvent<BeaconEvent>) => {
+            const message: ChannelMessage = event.detail.CM
+            const f = this._messageCallbackMap.get(message.Type)
+            if (f == undefined) {
+                this._defaultMessageCallback(message)
+            } else {
+                f(message)
+            }
+        })
+
+        this._beacon.addEventListener("CMU", (event: CustomEvent<BeaconEvent>) => {
+            const message: ChannelMetaUpdate = event.detail.CMU
+            if (message.Update == "Opened") {
+                this._onChannelOpened(message.Peer)
+            } else if (message.Update == "Closed") {
+                this._onChannelClosed(message.Peer)
+            }
+        })
+
+        this._nest = new Nest(socketAddr, RNMHandler);
 
     }
 
     registerMessage(type: string, callback: (cm: ChannelMessage) => void) {
-        this._messageMap[type] = callback
+        this._messageCallbackMap[type] = callback
     }
 
     registerDefault(callback: (cm: ChannelMessage) => void) {
         this._defaultMessageCallback = callback
     }
 
-    send(body?: any, type?: string, users?: string[]) {
-        const cm: ChannelMessage = {Type:type, Body:body}
+    onchannelopen(callback: (peer: string) => void) {
+        this._onChannelOpened = callback
+    }
 
+    onchannelclose(callback: (peer: string) => void) {
+        this._onChannelClosed = callback
+    }
+
+    send(cm: ChannelMessage, users?: string[]) {
         // if users is undefined or empty send message globally, otherwise send to specified user(s)
         if (users == undefined || users.length == 0) {
-            for(let [key, channel] of this._activeChannels) {
-                channel.send(cm)
+            for(const [key, channel] of this._activeChannels.entries()) {
+                channel.SCMProcessor(cm)
             }
         } else {
-            for(let user of users) {
-                let channel = this._activeChannels[user]
-                channel.send(cm)
+            for(const user of users) {
+                const channel = this._activeChannels.get(user)
+                channel.SCMProcessor(cm)
             }
         }
     }
